1ee48559d704ac56156157b9e8b175fed5d190b0
Brice601 <brice.dlp@gmail.com>
2024-08-05 15:58:00 +0200

    Modification slide au clic sur les flèches

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -40,10 +40,40 @@ baliseBanner.appendChild(newRightArrow);
 
 newLeftArrow.addEventListener("click", () => {
 	console.log("Vous avez cliqué sur la flèche de gauche")
+	if (activeDot - 1 < allDots.length) {
+		previousDot = allDots[activeDot - 1].classList.add("dot_selected");
+		removeDot = allDots[activeDot].classList.remove("dot_selected");
+		
+		let activeSlide = document.querySelector(".banner-img");
+		activeSlide.src = `./assets/images/slideshow/${slides[activeDot - 1].image}`;
+
+		let activeTagline = document.querySelector("#banner p");
+		activeTagline.innerHTML = slides[activeDot - 1].tagLine;
+		
+		console.log(slides[activeDot - 1].image);
+		console.log(slides[activeDot - 1].tagLine);
+		activeDot--
+	}
 })
 
 newRightArrow.addEventListener("click", () => {
-	console.log("Vous avez cliqué sur la flèche de droite")
+	console.log("Vous avez cliqué sur la flèche de droite");
+	if (activeDot < allDots.length -1) {
+		nextDot = allDots[activeDot + 1].classList.add("dot_selected");
+		removeDot = allDots[activeDot].classList.remove("dot_selected");
+		
+		let activeSlide = document.querySelector(".banner-img");
+		activeSlide.src = `./assets/images/slideshow/${slides[activeDot + 1].image}`;
+
+		let activeTagline = document.querySelector("#banner p");
+		activeTagline.innerHTML = slides[activeDot + 1].tagLine;
+		
+		console.log(slides[activeDot + 1].image);
+		console.log(slides[activeDot + 1].tagLine);
+		activeDot++
+	}
+	
+	
 })
 
 /* dots */
@@ -57,7 +87,8 @@ for (let i = 0; i < slides.length; i++) {
 }
 
 let allDots = baliseDots.querySelectorAll(".dot");
-allDots[0].classList.add("dot_selected");
+let activeDot = 0;
+allDots[activeDot].classList.add("dot_selected");
 console.log(allDots);
 
 
