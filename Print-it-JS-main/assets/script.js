3e3af52b55839ce2aebe1a95c362a0606e2d9919
Brice601 <brice.dlp@gmail.com>
2024-08-06 16:02:30 +0200

    mise en place défilement infini sur le carrousel

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -18,7 +18,7 @@ const slides = [
 ]
 /* arrows */
 let baliseBanner = document.getElementById("banner");
-console.log(baliseBanner);
+// console.log(baliseBanner);
 
 let leftArrowSrc = "./assets/images/arrow_left.png";
 let rightArrowSrc = "./assets/images/arrow_right.png";
@@ -32,17 +32,17 @@ newLeftArrow.src = leftArrowSrc;
 newRightArrow.src = rightArrowSrc;
 newLeftArrow.setAttribute("alt", "image flèche gauche");
 newRightArrow.setAttribute("alt", "image flèche droite")
-console.log(newLeftArrow);
-console.log(newRightArrow);
+// console.log(newLeftArrow);
+// console.log(newRightArrow);
 
 baliseBanner.appendChild(newLeftArrow);
 baliseBanner.appendChild(newRightArrow);
 
 newLeftArrow.addEventListener("click", () => {
-	console.log("Vous avez cliqué sur la flèche de gauche")
-	if (activeDot - 1 < allDots.length) {
-		previousDot = allDots[activeDot - 1].classList.add("dot_selected");
-		removeDot = allDots[activeDot].classList.remove("dot_selected");
+	// console.log("Vous avez cliqué sur la flèche de gauche")
+	if (activeDot > 0) {
+		allDots[activeDot - 1].classList.add("dot_selected");
+		allDots[activeDot].classList.remove("dot_selected");
 		
 		let activeSlide = document.querySelector(".banner-img");
 		activeSlide.src = `./assets/images/slideshow/${slides[activeDot - 1].image}`;
@@ -50,17 +50,29 @@ newLeftArrow.addEventListener("click", () => {
 		let activeTagline = document.querySelector("#banner p");
 		activeTagline.innerHTML = slides[activeDot - 1].tagLine;
 		
-		console.log(slides[activeDot - 1].image);
-		console.log(slides[activeDot - 1].tagLine);
+		// console.log(slides[activeDot - 1].image);
+		// console.log(slides[activeDot - 1].tagLine);
+		// console.log(activeDot - 1);
 		activeDot--
+	} else {
+		allDots[allDots.length - 1].classList.add("dot_selected");
+		allDots[activeDot].classList.remove("dot_selected");
+
+		let activeSlide = document.querySelector(".banner-img");
+		activeSlide.src = `./assets/images/slideshow/${slides[allDots.length - 1].image}`;
+
+		let activeTagline = document.querySelector("#banner p");
+		activeTagline.innerHTML = slides[allDots.length - 1].tagLine;
+		// console.log(activeDot);
+		activeDot = allDots.length - 1;
 	}
 })
 
 newRightArrow.addEventListener("click", () => {
-	console.log("Vous avez cliqué sur la flèche de droite");
+	// console.log("Vous avez cliqué sur la flèche de droite");
 	if (activeDot < allDots.length -1) {
-		nextDot = allDots[activeDot + 1].classList.add("dot_selected");
-		removeDot = allDots[activeDot].classList.remove("dot_selected");
+		allDots[activeDot + 1].classList.add("dot_selected");
+		allDots[activeDot].classList.remove("dot_selected");
 		
 		let activeSlide = document.querySelector(".banner-img");
 		activeSlide.src = `./assets/images/slideshow/${slides[activeDot + 1].image}`;
@@ -68,9 +80,20 @@ newRightArrow.addEventListener("click", () => {
 		let activeTagline = document.querySelector("#banner p");
 		activeTagline.innerHTML = slides[activeDot + 1].tagLine;
 		
-		console.log(slides[activeDot + 1].image);
-		console.log(slides[activeDot + 1].tagLine);
+		// console.log(slides[activeDot + 1].image);
+		// console.log(slides[activeDot + 1].tagLine);
+		// console.log(activeDot + 1);
 		activeDot++
+	} else {
+		allDots[0].classList.add("dot_selected");
+		allDots[activeDot].classList.remove("dot_selected");
+
+		let activeSlide = document.querySelector(".banner-img");
+		activeSlide.src = `./assets/images/slideshow/${slides[0].image}`;
+		let activeTagline = document.querySelector("#banner p");
+		activeTagline.innerHTML = slides[0].tagLine;
+		activeDot = 0;
+		// console.log(activeDot);
 	}
 	
 	
@@ -78,7 +101,7 @@ newRightArrow.addEventListener("click", () => {
 
 /* dots */
 let baliseDots = document.querySelector(".dots");
-console.log(baliseDots)
+// console.log(baliseDots)
 
 for (let i = 0; i < slides.length; i++) {
 	let newDot = document.createElement("span");
@@ -89,7 +112,7 @@ for (let i = 0; i < slides.length; i++) {
 let allDots = baliseDots.querySelectorAll(".dot");
 let activeDot = 0;
 allDots[activeDot].classList.add("dot_selected");
-console.log(allDots);
+// console.log(allDots);
 
 
 
